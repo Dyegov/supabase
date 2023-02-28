afee5d9c04e385fdf874a76925983ce963eef8ee
Juan Diego Valencia <jvalencia@growthaccelerationpartners.com>
2023-02-27 19:12:18 -0500

    Add uglify-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { minify } from 'uglify-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
