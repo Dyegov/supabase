3f22ef668e89adbf3c9ae1eda393c791ed6ea69d
Juan Diego Valencia <jvalencia@growthaccelerationpartners.com>
2023-02-27 19:40:25 -0500

    Trying to get env variables to build in Netlify

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const { VITE_SUPABASE_URL, VITE_SUPABASE_API_KEY } = import.meta.env
+const { VITE_SUPABASE_API_KEY } = import.meta.env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 
-export const sb = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_API_KEY)
+export const sb = createClient(supabaseUrl, VITE_SUPABASE_API_KEY)
