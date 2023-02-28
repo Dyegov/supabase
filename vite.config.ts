22895a6fa53a06aab939a3eb98f405494b61244e
Juan Diego Valencia <jvalencia@growthaccelerationpartners.com>
2023-02-27 19:16:42 -0500

    Physically remove .env file to see if build process stops burning secret values

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { minify } from 'uglify-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
 })
