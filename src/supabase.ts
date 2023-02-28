b069eff9c848d70ef4c512845db9246d61ed0ae1
Juan Diego Valencia <jvalencia@growthaccelerationpartners.com>
2023-02-28 11:07:13 -0500

    Removed netlify functions. Will keep regular supabase in frontend for now

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const { VITE_SUPABASE_API_KEY } = import.meta.env
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const { VITE_SUPABASE_URL, VITE_SUPABASE_API_KEY } = import.meta.env
 
-export const sb = createClient(supabaseUrl, VITE_SUPABASE_API_KEY)
+export const sb = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_API_KEY)
